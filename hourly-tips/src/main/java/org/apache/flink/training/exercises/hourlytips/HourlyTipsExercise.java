f8ce6d86b20617dd398f0de4443882186da2e70c
Andrea Callarelli <andrea@datachef.co>
2025-11-08 22:12:21 +0100

    exercise HourlyTips

--- hourly-tips/src/main/java/org/apache/flink/training/exercises/hourlytips/HourlyTipsExercise.java
+++ hourly-tips/src/main/java/org/apache/flink/training/exercises/hourlytips/HourlyTipsExercise.java
@@ -19,15 +19,27 @@
 package org.apache.flink.training.exercises.hourlytips;
 
 import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.training.exercises.common.datatypes.TaxiFare;
 import org.apache.flink.training.exercises.common.sources.TaxiFareGenerator;
 import org.apache.flink.training.exercises.common.utils.MissingSolutionException;
+import org.apache.flink.util.Collector;
+//import org.apache.flink.streaming.api.w
+
+import java.time.Duration;
 
 /**
  * The Hourly Tips exercise from the Flink training.
@@ -75,10 +87,45 @@ public class HourlyTipsExercise {
         // start the data generator
         DataStream<TaxiFare> fares = env.addSource(source);
 
-        // replace this with your solution
-        if (true) {
-            throw new MissingSolutionException();
-        }
+        // Add the timestamps
+        DataStream<TaxiFare> streamWithTimestamps = fares
+                .assignTimestampsAndWatermarks(
+                        WatermarkStrategy.<TaxiFare>forBoundedOutOfOrderness(Duration.ofMinutes(1))
+                                .withTimestampAssigner(new SerializableTimestampAssigner<TaxiFare>() {
+                                    @Override
+                                    public long extractTimestamp(TaxiFare fare, long recordTimestamp) {
+                                        return fare.getEventTimeMillis();  // must return event time in ms
+                                    }
+                                })
+                );
+
+//      sum tops per driver per hour using tumbling window
+        DataStream<Tuple3<Long, Long, Float>> hourlyMax = streamWithTimestamps.keyBy(element -> element.driverId)
+                .window(TumblingEventTimeWindows.of(Time.hours(1)))
+                .process(new ProcessWindowFunction<TaxiFare, Tuple3<Long, Long, Float>, Long, TimeWindow>() {
+                    @Override
+                    public void process(
+                            Long driverId,
+                            Context context,
+                            Iterable<TaxiFare> elements,
+                            Collector<Tuple3<Long, Long, Float>> out) throws Exception {
+                        float totalTips = 0;
+                        for (TaxiFare elem : elements) {
+                            if (elem.driverId == driverId) {
+                                totalTips += elem.tip;
+                            }
+                        }
+                        out.collect(Tuple3.of(context.window().getEnd(), driverId, totalTips));
+                    }
+                });
+
+//        Select top tipped driver. No need to key stream as we need to simply pick the
+//        element in the window that has the highest value for the tip
+        DataStream<Tuple3<Long, Long, Float>> topTippedDriverPerHour = hourlyMax
+                .windowAll(TumblingEventTimeWindows.of(Time.hours(1)))
+                .maxBy(2); // third element of the tuple is the summed up tip value
+
+        topTippedDriverPerHour.addSink(sink);
 
         // the results should be sent to the sink that was passed in
         // (otherwise the tests won't work)
